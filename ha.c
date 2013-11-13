7f5e35d77eefae5cf298070f0bc77c832cd2e920
Hugo <hugokallstrom@gmail.com>
2013-11-13 15:36:48 +0100

    Create ha.c

---
+++ ha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+	static const char filename[] = "file.txt";
+	FILE *file = fopen(filename, "r");
+
+	if (file != NULL ) {
+		char line[128];
+		while (fgets(line, sizeof line, file) != NULL ) {
+			fputs(line, stdout); /* write the line */
+		}
+		fclose(file);
+	}
+	else {
+		perror(filename); /* why didn't the file open? */
+	}
+	return 0;
+}
