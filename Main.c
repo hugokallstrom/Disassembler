cf66baf7a63b29ed607fbe501d94798a09e8e71e
oi11msd <oi11msd@cs.umu.se>
2013-11-14 11:38:15 +0100

    afd

--- Main.c
+++ Main.c
@@ -2,37 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+void readFile(FILE *file);
 int main(int argc, char *argv[]) {
 
-	readFile();
-	parseCodes();
+	FILE *file;
+
+    if (argc < 2) {
+            printf("<No file entered>\n");
+            exit(0);
+    }
+
+
+    /* Försöker öppna infilen */
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+            fprintf(stderr, "<Could not open file> %s\n", argv[1]);
+            exit(1);
+    }
+    /* Om filen som lästes in gick att öppna så
+    anropas funktionen readFile */
+    else {
+            readFile(file);
+    }
+	fclose(file);
+
+
+//	parseCodes();
 
 	return 0;
 }
 
-Array readFile(void) {
+void readFile(FILE *file) {
 
-	static const char filename[] = "file.txt";
-	FILE *file = fopen(filename, "r");
+	char line[128];
+	while (fgets(line, sizeof line, file) != NULL ) {
+		fputs(line, stdout); /* write the line */
+		/* If line == hex string
+		 * put the hex in an array containing only
+		 * hex strings */
 
-	if (file != NULL ) {
-		char line[128];
-		while (fgets(line, sizeof line, file) != NULL ) {
-			fputs(line, stdout); /* write the line */
-			/* If line == hex string
-			 * put the hex in an array containing only
-			 * hex strings */
+		/* else if line == binary string
+		 * put the string in an array containing only
+		 * binary strings */
 
-			/* else if line == binary string
-			 * put the string in an array containing only
-			 * binary strings */
-
-		}
-		fclose(file);
 	}
-	else {
-		perror(filename); /* why didn't the file open? */
-	}
-
-	return ;
 }
