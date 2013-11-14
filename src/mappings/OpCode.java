01ea33f55c729a744b6dbfc4b665e2154143982a
mattiashinnerson <mattias.hinnerson@gmail.com>
2013-11-14 15:09:51 +0100

    hej hej

--- src/mappings/OpCode.java
+++ src/mappings/OpCode.java
@@ -4,12 +4,45 @@ package mappings;
 
 public class OpCode {
 
-	public String opCodeName;
+	private String opCodeName;
+	private char opCodeType;
 
 	public OpCode(){
 
+		opCodeName = new String();
 
+	}
 
+	/**
+	 * method for setting a name to this opCode.
+	 * @param String name
+	 */
+	public void setOpCodeName(String name){
+		opCodeName = name;
+	}
+
+	/**
+	 * method for setting a type to this opCode.
+	 * @param Char OpType
+	 */
+	public void setOpCodeType(char OpType){
+		opCodeType = OpType;
+	}
+
+	/**
+	 * method for getting the opCode name
+	 * @return String OpCodeName
+	 */
+	public String getOpCodeName(){
+		return opCodeName;
+	}
+
+	/**
+	 * method for getting the opCode type
+	 * @return char OpCodeType
+	 */
+	public char getOpCodeType(){
+		return opCodeType;
 	}
 
 
