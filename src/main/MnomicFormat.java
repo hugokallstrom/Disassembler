53c85b2079ad0767d8fc076194833df466963bcb
oi11msd <oi11msd@cs.umu.se>
2013-11-15 11:54:41 +0100

    fd

--- src/main/MnomicFormat.java
+++ src/main/MnomicFormat.java
@@ -4,25 +4,26 @@ import mappings.OpCode;
 import mappings.OpMappings;
 
 public class MnomicFormat {
-	
-	
+
+
 	public static char getFormat(int op) {
 		if(op == 0) {
 			return 'R';
 		} else if(op == 28) {
 			return 'R';
 		}
-		
-		
+
+
 		OpCode temp = OpMappings.allOP[op];
-		
+
 		char format = temp.getOpCodeType();
-		
-		return format;		
-				
+
+		return format;
+
 	}
-	
+
 	public static String getMnomicFormat(char format, long number) {
+		String mnemonic = "";
 		if(format == 'R') {
 			String temp = Long.toBinaryString(number);
 			int op = Integer.parseInt(temp.substring(0, 6));
@@ -31,15 +32,15 @@ public class MnomicFormat {
 			int rd = Integer.parseInt(temp.substring(16, 21));
 			int shamt = Integer.parseInt(temp.substring(21, 26));
 			int funct = Integer.parseInt(temp.substring(26, 32));
-			
+
 			OpCode temp2 = OpMappings.allOP[op];
-			
+
 			String functName = OpMappings.RFunctions[funct];
 			String regNick1 = OpMappings.registerNicks[rs];
 			String regNick2 = OpMappings.registerNicks[rt];
 			String regNick3 = OpMappings.registerNicks[rd];
-			
-			String mnemonic = functName + " " + regNick3 + " " + regNick1 + " " + regNick2; 
+
+			mnemonic = functName + " " + regNick3 + " " + regNick1 + " " + regNick2;
 
 		} else if(format == 'I') {
 			String temp = Long.toBinaryString(number);
@@ -49,17 +50,36 @@ public class MnomicFormat {
 			int imm = Integer.parseInt(temp.substring(16, 32));
 
 			OpCode temp2 = OpMappings.allOP[op];
-			
+
 			String functName = temp2.getOpCodeName();
 			String regNick1 = OpMappings.registerNicks[rs];
 			String regNick2 = OpMappings.registerNicks[rt];
-			
-			String mnemonic = functName + " " + regNick2 + " " + regNick1; 
+
+			if(op .....) {
+
+			} else if(fdfdfsf) {
+
+			}else if (adfdf) {
+
+			}
+
+
+			mnemonic = functName + " " + regNick2 + " " + imm + regNick1;
 		} else if(format == 'J') {
-			
+			String temp = Long.toBinaryString(number);
+			int op = Integer.parseInt(temp.substring(0, 6));
+			int label = Integer.parseInt(temp.substring(6, 32));
+
+			OpCode temp2 = OpMappings.allOP[op];
+			String functName = temp2.getOpCodeName();
+
+			mnemonic = functName + " " + label;
 		}
+
+
+		return mnemonic;
 	}
-	
-	
-	
+
+
+
 }
